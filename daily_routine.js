e93eb34e3875429195ae386f473716e452b1d74c
sanjar <qulievsanjar3101@gmail.com>
2026-04-10 14:19:07 +0500

    html va css deyarli tayyor

--- daily_routine.js
+++ daily_routine.js
@@ -1,1 +1,6 @@
 "use strict"
+
+let todo_title = document.querySelector("#form_title");
+let todo_start_time = document.querySelector("#start");
+let todo_end_time = document.querySelector("#end");
+let todo_body = document.querySelector("#form_body");
